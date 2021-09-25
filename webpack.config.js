4c0b0226405732a53bcac0aacf7d7035357c5726
dymafr <contact@dyma.fr>
2021-09-25 10:04:04 +0200

    maj

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,7 @@ module.exports = {
   mode: 'development',
   devServer: {
     open: false,
-    contentBase: './dist',
-    inline: true,
+    static: path.resolve(__dirname, './dist'),
     port: 4000,
   },
 };
